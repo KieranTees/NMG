1620870dacaaa3d99b43385921782a6d7454ed2e
KieranTees <123987773+KieranTees@users.noreply.github.com>
2023-04-27 12:46:37 +0100

    Started ICA Project

---
+++ Project/include/Server.h
@@ -0,0 +1,17 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+
+class Server
+{
+    public:
+        Server();
+        virtual ~Server();
+        void run();
+
+    protected:
+
+    private:
+};
+
+#endif // SERVER_H
---
+++ Project/src/Server.cpp
@@ -0,0 +1,16 @@
+#include "Server.h"
+
+#include <iostream>
+
+Server::Server()
+{
+
+}
+Server::~Server()
+{
+
+}
+void Server::run()
+{
+    std::cout << "Hello" << std::endl;
+}
